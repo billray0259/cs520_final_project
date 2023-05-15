ce65faf57e8098c0c9e411fa7c44c03375211623
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2023-05-15 12:13:00 -0400

    Landing bp created and route created

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager, current_user
 
 from rater import app
 from rater.models import Climber
-from rater.views import climber_bp, gym_bp, route_bp
+from rater.views import climber_bp, gym_bp, route_bp, landing_bp
 
 
 # configure the login manager
@@ -21,13 +21,16 @@ def load_user(_id):
 app.register_blueprint(climber_bp)
 app.register_blueprint(gym_bp)
 app.register_blueprint(route_bp)
+app.register_blueprint(landing_bp)
 
 # define the index route
 @app.route('/')
 def index():
     if not current_user.is_authenticated:  # Check if user is not logged in
         print("User is not logged in")
-        return redirect(url_for('climber.login'))  # Redirect to sign-up page
+        # return redirect(url_for('climber.login'))  # Redirect to sign-up page
+        return redirect(url_for('landing.landing'))  # Redirect to sign-up page
+        # return
     elif current_user.gym_id is None:  # Check if user is logged in but doesn't have a selected gym
         print("User is logged in but has not selected a gym")
         return redirect(url_for('gym.search'))  # Redirect to gym page
--- src/rater/views/__init__.py
+++ src/rater/views/__init__.py
@@ -1,3 +1,4 @@
 from .climber import climber_bp
 from .gym import gym_bp
 from .route import route_bp
+from .landing import landing_bp
