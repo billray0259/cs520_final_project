7b6eb6e527f71781b50cc89e2f1b15855d95de24
Andrew Xie <alxie@umass.edu>
2023-05-18 20:05:26 -0400

    Created test.py

---
+++ test/test.py
@@ -0,0 +1,5 @@
+# Implement testing method utilizing the strategies listed in the below links:
+# https://docs.python.org/3/library/unittest.html
+# https://machinelearningmastery.com/a-gentle-introduction-to-unit-testing-in-python/
+# https://www.digitalocean.com/community/tutorials/how-to-use-unittest-to-write-a-test-case-for-a-function-in-python
+# https://www.dataquest.io/blog/unit-tests-python/
