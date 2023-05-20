d0c23f59336015c6572e7d847311e0b18058d74c
Andrew Xie <alxie@umass.edu>
2023-05-19 23:18:50 -0400

    Added test code for unittest

--- test/test.py
+++ test/test.py
@@ -3,3 +3,32 @@
 # https://machinelearningmastery.com/a-gentle-introduction-to-unit-testing-in-python/
 # https://www.digitalocean.com/community/tutorials/how-to-use-unittest-to-write-a-test-case-for-a-function-in-python
 # https://www.dataquest.io/blog/unit-tests-python/
+
+import unittest
+
+# Most important tests that are essential to the main functionality of the RouteRater app:
+# - One account per email address
+# - Invalid grades are not accepted
+# - 
+
+# Script copied from official python unittest website - Delete Later
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertNotEqual('foo'.upper(), 'FOo')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+        # self.assertTrue('Foo'.isupper()) # Expected to fail
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
