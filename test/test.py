7c3f17b453780be497eaa14442685bfa349a9d54
Andrew Xie <alxie@umass.edu>
2023-05-21 17:39:21 -0400

    Add and remove gym tests implemented

--- test/test.py
+++ test/test.py
@@ -23,6 +23,10 @@ def gen_email():
     return ''.join(random.choices(string.ascii_lowercase +
                              string.digits, k=7)) + "@email.com"
 
+def gen_gym_name():
+    return "Gym " + ''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=5))
+
 # Most important tests that are essential to the main functionality of the RouteRater app:
 # - Check all relationships specified in the ERD diagram
 # - One account per email address (Y)
@@ -40,46 +44,66 @@ def gen_email():
 #   - Should users be allowed to add gyms themselves?
 
 class TestRouteRater(unittest.TestCase):
-    # Test 1: Two climbers with different usernames cannot have the same email address
-    def test_email(self):
-        fixed_email = gen_email()
-        climber1 = Climber(fixed_email, gen_username(), ObjectId(), None, [], [])
-        climber1.set_password('password')
-        climber2 = Climber(fixed_email, gen_username(), ObjectId(), None, [], [])
-        climber2.set_password('password')
+#     # Test 1: Two climbers with different usernames cannot have the same email address
+#     def test_email(self):
+#         fixed_email = gen_email()
+#         climber1 = Climber(fixed_email, gen_username(), ObjectId(), None, [], [])
+#         climber1.set_password('password')
+#         climber2 = Climber(fixed_email, gen_username(), ObjectId(), None, [], [])
+#         climber2.set_password('password')
 
-        with self.assertRaises(ValueError):
-            climber1.save()
-            climber2.save()
+#         with self.assertRaises(ValueError):
+#             climber1.save()
+#             climber2.save()
 
 
-# Test 2: Two climbers with different email addresses cannot have the same username
-    def test_username(self):
-        fixed_username = gen_username()
-        climber1 = Climber(gen_email(), fixed_username, ObjectId(), None, [], [])
-        climber1.set_password('password')
-        climber2 = Climber(gen_email(), fixed_username, ObjectId(), None, [], [])
-        climber2.set_password('password')
+# # Test 2: Two climbers with different email addresses cannot have the same username
+#     def test_username(self):
+#         fixed_username = gen_username()
+#         climber1 = Climber(gen_email(), fixed_username, ObjectId(), None, [], [])
+#         climber1.set_password('password')
+#         climber2 = Climber(gen_email(), fixed_username, ObjectId(), None, [], [])
+#         climber2.set_password('password')
 
-        with self.assertRaises(ValueError):
-            climber1.save()
-            climber2.save()
+#         with self.assertRaises(ValueError):
+#             climber1.save()
+#             climber2.save()
 
-# Test 3: Two climbers with different email addresses and usernames can exist and DO NOT have the same ID
-    def test_diffemailanduser(self):
+# # Test 3: Two climbers with different email addresses and usernames can exist and DO NOT have the same ID
+#     def test_diffemailanduser(self):
+#         climber1 = Climber(gen_email(), gen_username(), ObjectId(), None, [], [])
+#         climber1.set_password('password')
+#         climber2 = Climber(gen_email(), gen_username(), ObjectId(), None, [], [])
+#         climber2.set_password('password')
+
+#         # Should not return ValueError
+#         climber1.save()
+#         climber2.save()
+
+# Test 4: Once a climber adds a favorite gym, it is stored in their favorite gyms (add gym method)
+    def test_addgym(self):
         climber1 = Climber(gen_email(), gen_username(), ObjectId(), None, [], [])
         climber1.set_password('password')
-        climber2 = Climber(gen_email(), gen_username(), ObjectId(), None, [], [])
-        climber2.set_password('password')
+        
+        gym1 = Gym(gen_gym_name(), "120 East St, Hadley, MA", ObjectId(), None, None, None, None)
+        climber1.add_favorite_gym(gym1)
 
-        # Should not return ValueError
-        climber1.save()
-        climber2.save()
+        self.assertTrue(gym1.id in climber1.gyms)
 
-# Test 4: Once a climber adds a favorite gym, it is stored in their favorite gyms 
+        num_gyms = len(climber1.gyms)
+        climber1.add_favorite_gym(gym1)
+        self.assertEqual(num_gyms, len(climber1.gyms))
 
 
-# Test 5: Same as test 4 but with unfavoriting gyms
+# Test 5: Same as test 4 but with unfavoriting gyms (remove gym method)
+    def test_removegym(self):
+        climber1 = Climber(gen_email(), gen_username(), ObjectId(), None, [], [])
+        climber1.set_password('password')
+        
+        gym1 = Gym(gen_gym_name(), "120 East St, Hadley, MA", ObjectId(), None, None, None, None)
+        climber1.add_favorite_gym(gym1)
+        climber1.remove_favorite_gym(gym1)
+        self.assertFalse(gym1.id in climber1.gyms)
 
 # Test 6: Each user must have an email address, username, and password
 
